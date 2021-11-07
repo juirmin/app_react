298ced080315b753aa96a797e6f42373dcf1b58a
juirmin <c108156120@nkust.edu.tw>
2021-11-08 00:22:04 +0800

    update viewport in npm

--- src/ch5/style.js
+++ src/ch5/style.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {StyleSheet} from 'react-native';
-import {vw, vh, vmin, vmax} from '../viewport';
+import {vw, vh, vmin, vmax} from 'react-native-viewport-use';
 const style = StyleSheet.create({
   title: {
     fontWeight: '900',
